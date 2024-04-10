bc6126af0f144cc6db9b2683fb124d30edc945cc
Levani1111 <papashvililevani@gmail.com>
2024-04-10 18:15:28 +0100

    Route handlers

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Importing the express module
-import express from 'express';
+import express from "express";
 
 // Importing data from JSON file with JSON import assertion
-import data from './data/mock.json' assert { type: 'json' };
+import data from "./data/mock.json" assert { type: "json" };
 
 // Creating an instance of the express application
 const app = express();
@@ -11,36 +11,48 @@ const app = express();
 const PORT = 3000;
 
 // Using the public folder at the root of the project as static folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Using images folder at the route /images as static folder
-app.use('/images', express.static('images'));
+app.use("/images", express.static("images"));
 
 // Handling GET request at '/'
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.json(data); // Sending the imported data as response
 });
 
+// Handling GET with next() function
+app.get(
+    "/next",
+    (req, res, next) => {
+        console.log("This response will be sent by the next middleware");
+        next(); // Calling the next middleware
+    },
+    (req, res) => {
+        res.send("This response will be sent by the second middleware");
+    }
+);
+
 // GET with Routing Parameters
-app.get('/class/:id', (req, res) => {
+app.get("/class/:id", (req, res) => {
     const studentId = Number(req.params.id); // Extracting the id from the URL
     const student = data.filter((student) => student.id === studentId); // Filtering the data based on the id
     res.send(student); // Sending the filtered data as response
 });
 
 // Handling POST request at '/create'
-app.post('/create', (req, res) => {
-    res.send('This is the POST request at /create');
+app.post("/create", (req, res) => {
+    res.send("This is the POST request at /create");
 });
 
 // Handling PUT request at '/edit'
-app.put('/edit', (req, res) => {
-    res.send('This is the PUT request at /edit');
+app.put("/edit", (req, res) => {
+    res.send("This is the PUT request at /edit");
 });
 
 // Handling DELETE request at '/delete'
-app.delete('/delete', (req, res) => {
-    res.send('This is the DELETE request at /delete');
+app.delete("/delete", (req, res) => {
+    res.send("This is the DELETE request at /delete");
 });
 
 // Listening to the specified port for incoming connections
